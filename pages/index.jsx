116429ebad1b90811eb0a90bc5c8bd4c93296a39
ashxcim <118277526+ashxcim@users.noreply.github.com>
2023-03-19 21:33:55 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -90,7 +90,7 @@ Vote Nuhz</a>
                   <p className="text-3xl text-white font-semibold">Best Your Bot</p>
                   <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to throw the best parties with Nuhz, the best and most up-to-date bot?</p>
                 </div>
-                <img src="https://cdn.discordapp.com/attachments/737348411568685066/983094813115170866/unknown.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
+                <img src="https://cdn.discordapp.com/attachments/1075544712255123487/1087081496248057916/image.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
               </>
       
         </div>
