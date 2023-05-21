483ada97a2365121be4229fbc700854119e7816a
ashxcim <118277526+ashxcim@users.noreply.github.com>
2023-05-21 16:15:49 +0300

    Update partners.jsx

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -16,7 +16,7 @@ export default function Partners() {
             <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
 
             <div className="flex flex-col justify-center text-white rounded">
-            <a href="https://discord.gg/VuxAwpEqws">
+            <a href="https://discord.gg/DkFB5gcb">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
                             <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/attachments/1102708044154011688/1109831307363614780/standard.gif" />
                             <div>
@@ -32,14 +32,14 @@ export default function Partners() {
                     </div>
 
                     <div className="flex flex-col justify-center text-white rounded">
-                        <a href="https://discord.gg/3PXwCzS6nF">
+                        <a href="https://discord.gg/pxBda9JnJv">
                             <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
                             <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/icons/924312493537509407/721de16575a9048c9c6980613ebb99f0.webp" />
                             <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">The Developers</h1>        
+                                <h1 className="leading-none text-3xl font-bold text-white">Hazırmısın?</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
-                                    You can get help on software and similar issues or you can spend time chatting. How about being a cool developer?
+                                    Yazılım ve benzeri konularda yardım alabilir veya sohbet ederek vakit geçirebilirsiniz. Harika bir geliştirici olmaya ne dersiniz?
                                     </p>
                                 </div>     
                             </div>
