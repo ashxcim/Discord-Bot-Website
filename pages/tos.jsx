d4db209d9a1859de79aa50e0642a94362265e9de
ashxcim <118277526+ashxcim@users.noreply.github.com>
2023-05-21 16:20:22 +0300

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -4,17 +4,17 @@ export default function Tos() {
      <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
       <p className="text-sm text-white/30 mb-1">Last Update: 20/05/2022 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-<p>⊳ Every discord server can use the Nuhz bot for free, it is completely prohibited to sell or market for a fee.</p>
+<p>⊳ Alisa botu her discord sunucusu ücretsiz kullanabilir, ücret karşılığında satması veya pazarlaması kesinlikle yasaktır.</p>
 <div className="py-3"></div>
-<p>⊳ You cannot use bot commands to harm the bot.</p>
+<p>⊳ Bot komutlarını bota zarar vermek için kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ You may not use open source codes to harm other servers.</p>
+<p>⊳ Açık kaynak kodlarını diğer sunuculara zarar vermek için kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ You may not sell or market open source codes through other platforms.</p>
+<p>⊳ Açık kaynak kodlarını diğer platformlar aracılığıyla satamaz veya pazarlayamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ While using the bot, you cannot use it in a way that harms other users for troll purposes.</p>
+<p>⊳ Botu kullanırken troll amaçlı diğer kullanıcılara zarar verecek şekilde kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ To use certain discord bot commands, you need to vote for the <a className="text-white" href="https://top.gg/user/180320834068819968">Nuhz</a> bot at certain intervals.</p>
+<p>⊳ Belirli discord bot komutlarını kullanmak için belirli zamanlarda <a className="text-white" href="https://top.gg/tr/bot/1087375299844509716">Alisa</a> botuna oy vermeniz gerekir. aralıklar.</p>
 </div>
     </div>
   );
