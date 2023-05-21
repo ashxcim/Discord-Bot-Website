0e17a848a5a3ab74a9292a7f517d764aad409121
ashxcim <118277526+ashxcim@users.noreply.github.com>
2023-05-21 16:24:07 +0300

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,29 +11,29 @@ class MyDocument extends Document {
           <meta name="googlebot" content="index,follow" />
           <meta
             name="description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="AlisaBOT 01.01.2022 Tarihinde Kurulun Bir Bottur .Moderasyon,Eğlence,Guard,Ve Daha Fazlası SizlerleSizden Ricam Lütfen Botumuza Oy Vererek Bize Çok Destek Olmuş Olursunuz Saygılarımla"
           />
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@NuhzBot" />
-          <meta name="twitter:creator" content="@NuhzBot" />
-          <meta property="og:url" content="https://nuhzBot.vercel.app" />
+          <meta name="twitter:site" content="@AlisaBot" />
+          <meta name="twitter:creator" content="@AlisaBot" />
+          <meta property="og:url" content="https://AlisaBot.vercel.app" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="NuhzBot" />
+          <meta property="og:title" content="AlisaBot" />
           <link
             rel="icon"
-            href="/img/logo2.png"
+            href="https://cdn.discordapp.com/attachments/1102685799226552341/1109139165754429490/Adsz_tasarm_-_2023-05-15T205940.953.png"
             type="image/x-icon"
           />
           <meta
             property="og:description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="AlisaBOT 01.01.2022 Tarihinde Kurulun Bir Bottur .Moderasyon,Eğlence,Guard,Ve Daha Fazlası SizlerleSizden Ricam Lütfen Botumuza Oy Vererek Bize Çok Destek Olmuş Olursunuz Saygılarımla"
           />
           <meta property="og:image" content="/img/logo.jpg" />
-          <meta property="og:image:alt" content="NuhzBot" />
+          <meta property="og:image:alt" content="AlisaBot" />
           <meta property="og:locale" content="en_GB" />
           <meta
             property="og:site_name"
-            content="NuhzBot"
+            content="AlisaBot"
           />
           <meta name="theme-color" content="#6108dd" />
           <link rel="icon" href="/img/logo.jpg" type="image/x-icon" />
