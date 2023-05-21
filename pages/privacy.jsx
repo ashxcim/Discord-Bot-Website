1b137de7976e5e6aa011c1affda81b173f340675
ashxcim <118277526+ashxcim@users.noreply.github.com>
2023-05-21 16:18:48 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -2,20 +2,20 @@ export default function Privacy() {
 
     return (
         <div>
-            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Nuhz</h1>
+            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Alisa</h1>
             <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ To use the Nuhz bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
+        <p>⊳ Alisa botu kullanmak için discord sunucunuza eklerken gerekli izinleri tanımlamanız gerekmektedir. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
         <div className="py-3"></div>
-        <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/user/180320834068819968">Nuhz</a> bot when using certain commands from the bot commands.</p>
+        <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/tr/bot/1087375299844509716">Alisa</a> bot when using certain commands from the bot commands.</p>
         <div className="py-3"></div>
-        <p>⊳ Every user can use the Nuhz bot however they want, as long as they do not intend to harm.</p>
+        <p>⊳ Alisa botu her kullanıcı zarar verme amacı taşımadığı sürece istediği gibi kullanabilir.</p>
         <div className="py-3"></div>
-        <p>⊳ You cannot use certain commands at the same time while using Nuhz.</p>   
+        <p>⊳ Alisa kullanırken belirli komutları aynı anda kullanamazsınız.</p>   
         <div className="py-3"></div>
-        <p>⊳ Nuhz bot does not save any data based on users.</p>
+        <p>⊳ Alisa bot, kullanıcı bazında herhangi bir veri kaydetmez.</p>
         <div className="py-3"></div>
-        <p>⊳ While using the bot, your user ID is saved and deleted in the system for 60 seconds for once.</p> 
+       <p>⊳ Botu kullanırken kullanıcı kimliğiniz bir defaya mahsus olmak üzere 60 saniye süreyle sisteme kaydedilir ve silinir.</p>
     
         </div>
             </div>
